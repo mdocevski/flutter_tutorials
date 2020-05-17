37d4de51cabc222e54578ec6155494259ba46004
Marko Dotsevski <mdocevski@gmail.com>
2020-05-18 00:25:57 +0200

    flash-chat add logout functionality

--- flash-chat-flutter/lib/screens/chat_screen.dart
+++ flash-chat-flutter/lib/screens/chat_screen.dart
@@ -39,6 +39,7 @@ class _ChatScreenState extends State<ChatScreen> {
           IconButton(
               icon: Icon(Icons.close),
               onPressed: () {
+                _auth.signOut().then((value) => Navigator.pop(context));
                 //Implement logout functionality
               }),
         ],
