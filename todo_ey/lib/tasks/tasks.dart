be9f798d38322e91283271a27d929f4b856d45bd
Marko Dotsevski <mdocevski@gmail.com>
2020-05-24 12:29:01 +0200

    todo_ey - initial design implemented

---
+++ todo_ey/lib/tasks/tasks.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatefulWidget {
+  static const String ID = 'tasks';
+
+  TasksScreen({Key key}) : super(key: key);
+
+  @override
+  _TasksScreenState createState() => _TasksScreenState();
+}
+
+class _TasksScreenState extends State<TasksScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+      backgroundColor: Colors.lightBlueAccent,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.only(
+              top: 96.0,
+              bottom: 32.0,
+              left: 32.0,
+              right: 32.0,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  child: Icon(
+                    Icons.list,
+                    size: 40.0,
+                    color: Colors.lightBlueAccent,
+                  ),
+                  radius: 32.0,
+                  backgroundColor: Colors.white,
+                ),
+                SizedBox(height: 24.0),
+                Text(
+                  'Todoey',
+                  style: TextStyle(
+                    fontSize: 48.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                SizedBox(height: 4.0),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 40.0),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
