b19e59b85afd47081f6d7e0bff6c42911d181eaa
Marko Dotsevski <mdocevski@gmail.com>
2020-05-24 22:25:18 +0200

    todo_ey add bottomsheet ui

--- todo_ey/lib/ui/tasks/add_task_sheet.dart
+++ todo_ey/lib/ui/tasks/add_task_sheet.dart
@@ -5,12 +5,65 @@ class AddTaskBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          'This is a bottom shett',
-        ),
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 40.0,
+        horizontal: 32.0,
       ),
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              'Add Task',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 32.0,
+                color: Colors.lightBlueAccent,
+              ),
+            ),
+            SizedBox(height: 4.0),
+            TextField(
+                autofocus: true,
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.lightBlueAccent,
+                ),
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 4.0,
+                      color: Colors.lightBlueAccent,
+                    ),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 4.0,
+                      color: Colors.lightBlueAccent,
+                    ),
+                  ),
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 4.0,
+                      color: Colors.lightBlueAccent,
+                    ),
+                  ),
+                )),
+            SizedBox(height: 24.0),
+            FlatButton(
+              color: Colors.lightBlueAccent,
+              onPressed: () {},
+              padding: EdgeInsets.symmetric(vertical: 16.0),
+              child: Text(
+                'Add',
+                style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ]),
     );
   }
 }
