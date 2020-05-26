aef4a27d93e7300d1f545f307c180474c56996f3
Marko Dotsevski <mdocevski@gmail.com>
2020-05-26 21:10:59 +0200

    Revert "flash-chat add firebase integration to iOS project"
    This reverts commit ae672ba3d5727f15c9e416198a7c7c1afc8974f4.

--- flash-chat-flutter/ios/Flutter/flutter_export_environment.sh
+++ flash-chat-flutter/ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/marko.docevski/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/marko.docevski/FlutterProjects/flutter_tutorials/flash-chat-flutter"
-export "FLUTTER_TARGET=/Users/marko.docevski/FlutterProjects/flutter_tutorials/flash-chat-flutter/lib/main.dart"
+export "FLUTTER_ROOT=C:\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Projects\flash-chat-flutter"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=/Users/marko.docevski/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
-export "TRACK_WIDGET_CREATION=true"
